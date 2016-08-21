bb41e3cb8849320a693623a8f0deea46bf4e73fa
somyatuteja <somya16055@iiitd.ac.in>
2016-08-21 15:19:18 +0530

    Add files via upload

---
+++ TestingGit.java
@@ -0,0 +1,12 @@
+
+package testinggit;
+
+public class TestingGit {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Trying to create Git repository");
+        System.out.println("yo yo");
+    }
+    
+}
